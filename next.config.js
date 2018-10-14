73fbd4daa317281eb80410e3d2190492fbaf144e
vespa <danilo.vespa@dafiti.com.br>
2018-10-14 18:25:58 -0300

    downgrading next to avoid the new version sass conflict. Add Sass structure

--- next.config.js
+++ next.config.js
@@ -1,15 +1,30 @@
 const withSass = require('@zeit/next-sass');
 const withCss = require('@zeit/next-css');
 const withImages = require('next-images');
+const commonsChunkConfig = require('@zeit/next-css/commons-chunk-config');
 
-module.exports = withSass(
-  withImages(
-    withCss({
-      cssModules: true,
-      cssLoaderOptions: {
-        importLoaders: 1,
-        localIdentName: '[local]___[hash:base64:5]',
-      },
-    }),
-  ),
-);
+function addPolyfill(config) {
+  const originalEntry = config.entry;
+  /* eslint-disable */
+  config.entry = async () => {
+    const entries = await originalEntry();
+    if (entries['main.js']) {
+      entries['main.js'].unshift('babel-polyfill');
+    }
+    return entries;
+  };
+}
+
+const conf = {
+  cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: "[local]___[hash:base64:5]",
+  },
+  webpack(config) {
+    addPolyfill(config);
+    return commonsChunkConfig(config, /\.(sass|scss|css)$/);
+  },
+};
+
+module.exports = withSass(withImages(withCss(conf)));
