ffaf5cf60ab4b17d97138f907b5edde4dfe5f20e
Unknown <danilo.vespa@dafiti.com.br>
2018-10-16 01:46:30 -0300

    Added Mobile Behavior to Header

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Image from '../../elements/Image';
 import classes from './style.scss';
 
 const Header = ({ currentBalance, customer, date }) => (
   <header className={classes.header}>
-    <div>
-      <Image src="neon-logo-02" />
-    </div>
-    <div className={classes.header__balance}> Saldo: {currentBalance}</div>
+    <div className={classes.header__logo} />
+    <div className={classes.header__balance}> saldo<span> {currentBalance}</span></div>
     <div />
     <div />
     <div>
       {date}
       <div className={classes.header__separator} />
-      Bem vindo, {customer}
+      Bem vindo<span>, {customer}</span>
     </div>
   </header>
 );
