16bdd1a70bac16fee9f66b06076ac919a8249624
Unknown <danilo.vespa@dafiti.com.br>
2018-10-16 03:20:29 -0300

    Adding slide animation

--- components/Slider/index.jsx
+++ components/Slider/index.jsx
@@ -7,20 +7,50 @@ class Slider extends React.PureComponent {
     children: PropTypes.node.isRequired,
   }
 
+  constructor(props) {
+    super(props);
+    this.ref = React.createRef();
+    this.state = {
+      active: 0,
+    };
+    this.isCurrent = this.isCurrent.bind(this);
+  }
+
   isCurrent() {
-    return true;
+    const { children } = this.props;
+    const { active } = this.state;
+    const totalChilds = children.length;
+    const maxScrollLeft = this.ref.current.scrollWidth;
+    const divisions = maxScrollLeft / totalChilds;
+    // const left = this.ref.current.scrollLeft;
+    // this.ref.current.scrollLeft = 999;
+    console.log(divisions);
+    setTimeout(()=>{
+      this.ref.current.style.left = -(divisions) + "px";
+    });
+    this.setState({
+      active: active + 1,
+    })
+    
   }
 
   render() {
     const { children } = this.props;
+    const { active } = this.state;
+    let acitveClass;
     let count = 0;
     return (
-      <ul className={classes.slider}>
-        {children.map((item) => {
-          count += 1;
-          return <li key={count}> {item}</li>;
-        })}
-      </ul>
+      <div className={classes.slider__container}>
+        <div className={classes.slider} onTouchEnd={this.isCurrent}>
+          <ul className={classes.slider} ref={this.ref}>
+            {children.map((item, i) => {
+              acitveClass = i === active ? classes.active : ''
+              count += 1;
+              return <li key={count} className={acitveClass}> {item}</li>;
+            })}
+          </ul>
+        </div>
+      </div>
     );
   }
 }
