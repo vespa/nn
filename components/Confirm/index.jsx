19b0b0bf41302f4202fcb72a13c5dcb611890bb8
Unknown <danilo.vespa@dafiti.com.br>
2018-10-15 23:08:01 -0300

    Confirme component styled

--- components/Confirm/index.jsx
+++ components/Confirm/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classes from './style.scss';
 
 const Message = ({ message, callback, cancel }) => {
   return (
     <React.Fragment>
-      <div>
-        {message}
-        <button type="button" onClick={cancel}>cancela</button>
-        <button type="button" onClick={callback}> confirma </button>
+      <div className={classes.message__blackbox} onClick={cancel} role="presentation" />
+      <div className={classes.message}>
+        <div className={classes.message__text}>
+          {message}
+        </div>
+        <button type="button" onClick={cancel} className="btn btn--white right">cancela</button>
+        <button type="button" onClick={callback} className="btn btn--white right"> confirma </button>
       </div>
     </React.Fragment>
   );
