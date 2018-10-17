d731ccf386e3595dfd861e20f835d427bdd66bcd
Unknown <danilo.vespa@dafiti.com.br>
2018-10-16 23:51:13 -0300

    improving Header coverage test

---
+++ components/Header/index.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from './index';
+
+describe('[Component: List]', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(
+      <Header currentBalance="1,00" customer="Juca" date={2018} deletable />,
+    );
+  });
+
+  it('should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
